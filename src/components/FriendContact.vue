b457d4ab9d30c4a920da235de11e12123844bd1e
Malak Krayem <70662779+MalakKrayem@users.noreply.github.com>
2021-09-22 11:26:19 +0300

    Initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,76 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite ? "(Favourite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFaveroite">Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  //  props: ["name", "phoneNumber", "emailAddress", "isFavourite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function (value) {
+      //   return value === "1" || value === "0";
+      // },
+    },
+  },
+  emits: ["toggle-faveroite", "delete"],
+  /* emits: {
+    "toggle-faveroite": function (id) {
+      if (id) {
+        return true;
+      } else {
+        console.log("Id is missing!");
+        return false;
+      }
+    },
+  }, */
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFaveroite() {
+      this.$emit("toggle-faveroite", this.id);
+    },
+  },
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form @submit.prevent="add">
+      <div>
+        <label>Name</label>
+        <input type="=text" v-model="name" />
+      </div>
+      <div>
+        <label>Phone</label>
+        <input type="=tel" v-model="phone" />
+      </div>
+      <div>
+        <label>E-mail</label>
+        <input type="=email" v-model="email" />
+      </div>
+      <div>
+        <button>Add Contact</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      name: "",
+      phone: "",
+      email: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("add-contact", this.name, this.phone, this.email);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
